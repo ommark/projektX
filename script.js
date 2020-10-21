62569b6839591dc79abc7b9038304b5bbd86fe2a
Janeli <6unli@users.noreply.github.com>
2020-10-22 02:37:54 +0300

    Lisain majutuse lehe, väljade kontrolli. Kohendasin igast reavahesid ja muud nipet-näpet
    Ja leidsin et ma ei saanud varem commitida, sest polnud kokkuvõtet lisanud...

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Väljade kontroll
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName("needs-validation");
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+    if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+    }, false);
+    });
+    }, false);
+    })();
+
+
+
+// Kaart
+
 var mapAPIKey = "AuCOgDeVgO4-6Fc6-6ZJCh08O10BhG_e8-xa13Ci_8W4Z_qrRrgVdjUkp4gIUTi1";
 var map;
 
